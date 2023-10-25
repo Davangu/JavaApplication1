3ee21b7a31072eeb56c8296efb276beaf8630fa2
David <d.antolingutierrez@edu.gva.es>
2023-10-25 16:11:51 +0200

    Second commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author antolin
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     * Esto es una función
+     */
+    
+    static int a,b;
+
+    
+    public static void main(String[] args) {
+        int ejemploDesarrolloTema2;
+        ejemploDesarrolloTema2=3;
+        if (a==b) {
+           b = 10;
+        }               
+        
+        // TODO code application logic here
+    }
+    
+}
